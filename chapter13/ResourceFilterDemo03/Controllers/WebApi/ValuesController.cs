3a68cfd46cf5c711ba28e991748ad67df2ac66de
Wagner Xu <wagnerhsu@hotmail.com>
2019-03-30 19:09:46 +0800

    Add files

---
+++ chapter13/ResourceFilterDemo03/Controllers/WebApi/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResourceFilterDemo03.Infrastructure;
+
+namespace ResourceFilterDemo03.Controllers.WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [FeatureEnabled(IsEnabled = false)]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        [FeatureEnabled(IsEnabled = true)]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+    }
+}
