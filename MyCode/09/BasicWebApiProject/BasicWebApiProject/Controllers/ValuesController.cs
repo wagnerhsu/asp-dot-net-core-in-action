6d304133ee11c30f535de0b2f59628e182bacb60
Wagner Xu <wagnerhsu@hotmail.com>
2019-08-04 14:38:16 +0800

    Update files

---
+++ MyCode/09/BasicWebApiProject/BasicWebApiProject/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BasicWebApiProject.Controllers
+{
+    public class FruitController : Controller
+    {
+        private readonly List<string> _fruit = new List<string>
+            {
+                "Pear",
+                "Lemon",
+                "Peach"
+            };
+
+        public IEnumerable<string> Index()
+        {
+            return _fruit;
+        }
+
+        public IActionResult View(int id)
+        {
+            if (id >= 0 && id < _fruit.Count)
+            {
+                return Ok(_fruit[id]);
+            }
+
+            return NotFound();
+        }
+    }
+}
